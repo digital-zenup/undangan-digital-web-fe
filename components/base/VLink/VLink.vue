11a1833d25ecf1730a34d6f95a8dcb833d757211
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-01-24 18:24:11 +0700

    feat(link): create `VLink` component

---
+++ components/base/VLink/VLink.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { Props } from '.';
+import { NuxtLink } from '#components';
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <VButton
+        :as="NuxtLink"
+        v-bind="props"
+    >
+        <slot />
+    </VButton>
+</template>

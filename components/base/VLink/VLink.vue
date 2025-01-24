55d53d551ed32427f8e752797b836c99b3989eb2
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-01-24 18:51:36 +0700

    feat(link): create `VLink` component, thus applying it

--- components/base/VLink/VLink.vue
+++ components/base/VLink/VLink.vue
@@ -3,12 +3,28 @@ import type { Props } from '.';
 import { NuxtLink } from '#components';
 
 const props = defineProps<Props>();
+
+const vLinkProps = computed(() => {
+    const { prefetch, noPrefetch, ...rest } = props;
+
+    if (prefetch && noPrefetch) {
+        console.warn(
+            '[VLink] `prefetch` and `noPrefetch` cannot be used together. `noPrefetch` will be ignored.',
+        );
+    }
+
+    return {
+        ...rest,
+        prefetch: prefetch || undefined,
+        noPrefetch: prefetch ? false : noPrefetch,
+    };
+});
 </script>
 
 <template>
     <VButton
         :as="NuxtLink"
-        v-bind="props"
+        v-bind="vLinkProps"
     >
         <slot />
     </VButton>
